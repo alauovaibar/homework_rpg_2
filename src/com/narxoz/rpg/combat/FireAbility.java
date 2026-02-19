7b20201d784e125cf4b495caeb5e378de3d10ef6
Aibar Alauov <alauovajbar@gmail.com>
2026-02-19 21:55:36 +0500

    hw 2

--- src/com/narxoz/rpg/combat/FireAbility.java
+++ src/com/narxoz/rpg/combat/FireAbility.java
@@ -1,6 +1,9 @@
 package com.narxoz.rpg.combat;
 
 public class FireAbility  implements Ability{
+    public FireAbility(String flameBreath) {
+    }
+
     public void cast(){
         System.out.println("Use:fire breathing");
     }
--- src/com/narxoz/rpg/factory/FireEnemyFactory.java
+++ src/com/narxoz/rpg/factory/FireEnemyFactory.java
@@ -1,4 +1,24 @@
 package com.narxoz.rpg.factory;
 
-public class FireEnemyFactory {
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.FireAbility;
+import com.narxoz.rpg.loot.LootTable;
+import com.narxoz.rpg.loot.FireLoot;
+import java.util.Arrays;
+import java.util.List;
+
+public class FireEnemyFactory implements EnemyComponentFactory {
+    @Override
+    public List<Ability> createAbilities() {
+        return Arrays.asList(new FireAbility("Flame Breath"), new FireAbility("Meteor"));
+    }
+    @Override
+    public LootTable createLootTable() {
+        return new FireLoot();
+    }
+
+    @Override
+    public String createAIBehavior() {
+        return "AGGRESSIVE";
+    }
 }
