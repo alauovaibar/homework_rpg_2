9b669c84a99972ac566edbc086079c2f75f87450
Aibar Alauov <alauovajbar@gmail.com>
2026-02-19 21:56:52 +0500

    hw 2

--- src/com/narxoz/rpg/combat/IceAbility.java
+++ src/com/narxoz/rpg/combat/IceAbility.java
@@ -1,6 +1,9 @@
 package com.narxoz.rpg.combat;
 
 public class IceAbility  implements Ability{
+    public IceAbility(String frostNova) {
+    }
+
     public void cast(){
         System.out.println("Use:Ice storm");
     }
--- src/com/narxoz/rpg/factory/IceEnemyFactory.java
+++ src/com/narxoz/rpg/factory/IceEnemyFactory.java
@@ -1,4 +1,25 @@
 package com.narxoz.rpg.factory;
 
-public class IceEnemyFactory {
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.IceAbility;
+import com.narxoz.rpg.loot.LootTable;
+import com.narxoz.rpg.loot.IceLoot;
+import java.util.Arrays;
+import java.util.List;
+
+public class IceEnemyFactory implements EnemyComponentFactory {
+    @Override
+    public List<Ability> createAbilities() {
+        return Arrays.asList(new IceAbility("Frost Nova"), new IceAbility("Ice Shield"));
+    }
+
+    @Override
+    public LootTable createLootTable() {
+        return new IceLoot();
+    }
+
+    @Override
+    public String createAIBehavior() {
+        return "DEFENSIVE";
+    }
 }
